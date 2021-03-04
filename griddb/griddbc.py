fe74c8f160fbbd6c793f39288f6bae01f401bc3a
Leonardo <32509382+leonardouniromasalvucci@users.noreply.github.com>
2021-03-04 17:02:37 +0100

    upload files

---
+++ griddb/griddbc.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+
+import paho.mqtt.client as mqtt
+import griddb_python as griddb
+import pika, logging, os, sys, datetime, json, time
+
+logging.basicConfig(level=logging.INFO)
+LOG = logging.getLogger(__name__)
+
+try:
+        machine_ip = os.environ['RUBBITMQ_HOST_IP']
+except:
+        LOG.error('Error during the processing of the HOST IP')
+
+col = None
+factory = None
+
+update = True
+connected = False
+
+LOG.info('Trying to connect to GridDB...')
+
+while True:
+        try:
+                factory = griddb.StoreFactory.get_instance()
+                gridstore = factory.get_store(
+                notification_member="10.0.0.28:10001,10.0.0.37:10001,10.0.0.172:10001",
+                cluster_name="defaultCluster",
+                username="admin",
+                password="admin")
+
+                conInfo = griddb.ContainerInfo("SensorValuesKalpaLast",
+                                [["timestamp", griddb.Type.TIMESTAMP],
+                                ["sensorId", griddb.Type.STRING],
+                                ["sensorValue", griddb.Type.STRING]],
+                                griddb.ContainerType.TIME_SERIES, True)
+
+                col = gridstore.put_container(conInfo)
+                LOG.info('CONNCTED to GridDB.....')
+                break
+
+        except griddb.GSException as e:
+                LOG.error('Error during connection to GridDB.....')
+
+
+def callback(ch, method, properties, body):
+        y = json.loads(str(body.decode()))
+        LOG.info(" [x] Received %r" % y)
+        while True:
+                try:
+                        now = datetime.datetime.utcnow()
+                        res = col.put([now, str(y["device_id"]), str(y["value"])])
+                        LOG.info("GridDB reply: " + res)
+                        ch.basic_ack(delivery_tag=method.delivery_tag)
+                        break
+                except:
+                        LOG.error("Error during update gridDB. I'll try again")
+while True:
+        try:
+                connection = pika.BlockingConnection(pika.ConnectionParameters(host=machine_ip, port=5672))
+                channel = connection.channel()
+
+                channel.queue_declare(queue='kalpa_queue', durable=True)
+                LOG.info(' [*] Waiting for messages. To exit press CTRL+C')
+
+                channel.basic_qos(prefetch_count=1)
+                channel.basic_consume(queue='kalpa_queue', on_message_callback=callback)
+                channel.start_consuming()
+                break
+        except:
+                LOG.error("Connection error with RubbitMQ server.")
+
+
---
+++ producer/producer.py
@@ -0,0 +1,69 @@
+import pika, logging, sys, os, socket, time
+import paho.mqtt.client as mqtt
+
+logging.basicConfig(level=logging.INFO)
+LOG = logging.getLogger(__name__)
+
+try:
+    local_ip = socket.gethostbyname(socket.gethostname())
+    machine_ip = os.environ['RUBBITMQ_HOST_IP']
+    mqtt_id = machine_ip+''+local_ip
+except:
+    LOG.error('Error during the processing of the HOST IP')
+
+topic = '$share/group1/0001/'
+channel = None
+q_name = None
+rabbit_queue = None
+MAX_MESSAGES = 5
+
+def on_connect(client, userdata, flags, rc):
+    client.subscribe(topic, qos=2)
+    LOG.info('MQTT: Process is connected and it is ready to subscribe.')
+
+def on_message(client, userdata, message):
+    LOG.info('MQTT: ' + str(message.payload.decode("utf-8")))
+    while True:
+        try:
+            rabbit_queue = channel.queue_declare(queue='kalpa_queue', durable=True, passive=True)
+            q_l = rabbit_queue.method.message_count
+            channel.basic_publish(
+                exchange='',
+                routing_key='kalpa_queue',
+                body=str(message.payload.decode("utf-8")),
+                properties=pika.BasicProperties(delivery_mode = 2)
+            )
+
+            if(int(q_l) >= (MAX_MESSAGES-1)):
+                client.unsubscribe(topic)
+                while True:
+                    time.sleep(1)
+                    LOG.info('Waiting for publishing...')
+                    q_ll = rabbit_queue.method.message_count
+                    if(int(q_ll) <= (MAX_MESSAGES/2)):
+                        client.subscribe(topic, qos = 2)
+                        break
+            break
+        except:
+            LOG.error('Error during update RubbitMQ queue!!! Try again...')
+
+LOG.info('Starting connection with RubbitMQ server...')
+while True:
+    try:
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host = machine_ip, port = 5672))
+        channel = connection.channel()
+        break
+    except:
+        LOG.error('Connection error with RubbitMQ server.')
+
+LOG.info('Starting connection with MQTT Broker...')
+while True:
+    try:
+        client = mqtt.Client(client_id = mqtt_id, clean_session = False)
+        client.on_connect = on_connect
+        client.on_message = on_message
+        client.connect('InternalKalpaELB-c6dcbc9047674e10.elb.eu-west-1.amazonaws.com')
+        client.loop_forever()
+        break
+    except:
+        LOG.error('Connection error with MQTT Broker.')
