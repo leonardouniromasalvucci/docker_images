6ea10ef8aafe8cae70aa486e154c1da2bc20efd0
Leonardo <32509382+leonardouniromasalvucci@users.noreply.github.com>
2021-04-13 19:58:59 +0200

    UPD

--- rest-ws/rest-ws.py
+++ rest-ws/rest-ws.py
@@ -15,6 +15,8 @@ update = True
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+messages_sent_by_clients = 0
+
 def append_to_list(list, label, value):
     if (not any(label in d['target'] for d in list)):
         list.append({"target": label, "datapoints": [value]})
@@ -28,6 +30,70 @@ def to_timestamp(date):
     time_tuple = date.timetuple()
     timestamp = time.mktime(time_tuple)
     return timestamp
+
+@app.route('/<homeid>/monitoring', methods=['GET'])
+def monitoring(homeid):
+        global messages_sent_by_clients
+
+        try:
+                factory = griddb.StoreFactory.get_instance()
+                gridstore = factory.get_store(
+                        notification_member="10.0.0.28:10001,10.0.0.37:10001,10.0.0.172:10001",
+                        cluster_name="defaultCluster",
+                        username="admin",
+                        password="admin"
+                )
+
+                LOG.info("[MultiGet S] HOME ID = " + homeid)
+
+                results = []
+
+                listCon = []
+                listQuery = []
+                i=0
+                while True:
+                        try:
+                                container = gridstore.get_container("home-"+homeid+"_device-"+str(i))
+                                if container == None:
+                                        LOG.info("container: None")
+                                listCon.append(container)
+                                query = container.query("select * where timestamp > TIMESTAMPADD(MINUTE, NOW(), -30)")
+                                if query == None:
+                                        LOG.info("query: None")
+                                listQuery.append(query)
+                                LOG.info("home-"+homeid+"_device-" + str(i))
+                                i=i+1
+                        except:
+                                break
+                        
+                gridstore.fetch_all(listQuery)
+                for q in listQuery:
+                        rs = q.get_row_set()
+                        while rs.has_next():
+                                row = rs.next()
+                                results.append(row)
+                                LOG.info(row)
+
+                LOG.info("[MultiGet E]")
+
+        except griddb.GSException as e:
+                for i in range(e.get_error_stack_size()):
+                        LOG.err("[", i, "]")
+                        LOG.err(e.get_error_code(i))
+                        LOG.err(e.get_message(i))
+
+                        
+        html = '<table style="width:30%"><tr><th>Sent</th><th>Stored</th></tr>'
+        html += '<tr><td>'+messages_sent_by_clients+'</td><td>'+len(results)+'</td></tr>'
+        html += '</table>'
+        return html, 200
+
+
+@app.route('/increment', methods=['GET'])
+def increment():
+        global messages_sent_by_clients
+        messages_sent_by_clients = messages_sent_by_clients+1
+        return 'Success!', 200
 
 @app.route('/', methods=['GET'])
 def home():
